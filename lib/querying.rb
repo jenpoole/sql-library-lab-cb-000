74c617e3e6f919ce543649008a69781b1b637558
Jen Poole <contact.vandepool@gmail.com>
2018-02-17 20:13:44 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,12 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name
+  FROM authors
+  JOIN series
+  ON authors.id = series.author_id
+  JOIN subgenres
+  ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
@@ -41,5 +46,5 @@ end
   In SQLite the Count(*) function will return total number of rows available in a table, including the rows which contains NULL values. The Count(*) will not take any parameters other than asterisk symbol (*).
 
   source: http://tutlane.com/tutorial/sqlite/sqlite-count-function
-  
+
 =end
