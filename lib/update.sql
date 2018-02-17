f4a063835ec8f1c40fe2edffecccc1d89a40e9ac
Jen Poole <contact.vandepool@gmail.com>
2018-02-17 19:58:14 +0000

    Done.

---
+++ lib/update.sql
@@ -0,0 +1,8 @@
+-- updates the species of the last character in the characters table to "Martian"
+UPDATE characters
+SET species = 'Martian'
+WHERE id = 8;
+
+-- UPDATE table_name
+-- SET column1 = value1, column2 = value2...., columnN = valueN
+-- WHERE [condition];
